a2f79e1bd46068082dfcbc27fc8183f32a880577
Nussbaumer Alex <nuss.alex96@gmail.com>
2018-09-27 21:25:43 +0200

    Setting up the project arcitecture.

---
+++ dao/src/main/java/hu/uni/miskolc/iit/software_testing/App.java
@@ -0,0 +1,13 @@
+package hu.uni.miskolc.iit.software_testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
