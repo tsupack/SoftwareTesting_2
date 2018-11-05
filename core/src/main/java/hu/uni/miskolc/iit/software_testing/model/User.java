d1f7c21e630f5e76cf310b847f50107ef4a62042
mongooseasd <mongooseasd@gmail.com>
2018-11-05 17:02:57 +0100

    Rent management service implementation unit test start

--- core/src/main/java/hu/uni/miskolc/iit/software_testing/model/User.java
+++ core/src/main/java/hu/uni/miskolc/iit/software_testing/model/User.java
@@ -55,7 +55,7 @@ public class User {
     return "User{" +
             "id=" + id +
             ", userName='" + userName + '\'' +
-            ", adress='" + address + '\'' +
+            ", address='" + address + '\'' +
             ", phoneNumber=" + phoneNumber +
             '}';
   }
