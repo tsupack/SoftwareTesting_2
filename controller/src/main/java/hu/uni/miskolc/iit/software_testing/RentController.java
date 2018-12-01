47149e12fa13aade132f436a6efcaf38b8d4868d
mongooseasd <mongooseasd@gmail.com>
2018-12-01 15:37:28 +0100

    Structure

--- controller/src/main/java/hu/uni/miskolc/iit/software_testing/RentController.java
+++
@@ -1,8 +0,0 @@
-package hu.uni.miskolc.iit.software_testing;
-
-public class RentController {
-/*
-    TODO: Create requestmappings for the possible cases like: getAll, update, delete, getRent/{rentId},
-    getRent/{dateStart} {dateEnd} ... etc
- */
-}
--- controller/src/main/java/hu/uni/miskolc/iit/software_testing/UserManagementController.java
+++
@@ -1,9 +0,0 @@
-package hu.uni.miskolc.iit.software_testing;
-
-public class UserManagementController {
-
-    /*
-    TODO: Requestmappings for possible requests like getAll, getUser, etc...
-     */
-
-}
